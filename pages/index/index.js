6c74af9ddb11c97a2824a7deed2b618364eae34a
libp <924701358@qq.com>
2022-01-11 22:21:41 +0800

    首页搞定。但是用户首次加载相关的随机推荐、作者名获取功能还可以优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -110,17 +110,69 @@ Page({
         'dataType': 'json'
       },
       success: function (res) {
-        if(res.data.author==2){
-          res.data.author = '任正非'
-        }else if(res.data.author==3){
-          res.data.author = '毛泽东'
-        }
         that.setData({
-          title: res.data.title.rendered.split('_')[1],
-          author: res.data.author,
-          article: res.data.content.rendered,
-          publish_date: res.data.title.rendered.split('_')[0],
+          title: res.data.title.rendered,
+          // author: res.data.author,
+          article: res.data.content.rendered.replace(/<p/g, '<p style="margin:0% 0% 5% 0%;" ').replace(/<h2/g, '<h2 style="display:none" ').replace(/<h1/g, '<h1 style="display:none" '),
+          publish_date: res.data.date,
           id: res.data.id,
+        });
+        that.checkLocalUserList(res.data.author);
+        wx.hideLoading();
+        wx.hideNavigationBarLoading();
+        }
+    })
+  },
+  checkLocalUserList(id){
+    let that = this;
+    let userList = wx.getStorageSync('userList');
+    // console.log(userList)
+    for ( var i = 0; i <userList.length; i++){
+      if(userList[i].id==id){
+        that.setData({
+          author: userList[i].name,
+        })
+        wx.hideLoading()
+        wx.hideNavigationBarLoading();
+        break
+      }
+    }
+
+    if(userList.length>0){
+      var userInList = 0
+      for (let i = 0, len = userList.length; i < len; i++) {
+        if(userList[i].id==id){
+          that.setData({
+            author: userList[i].name,
+          })
+          wx.hideLoading()
+          wx.hideNavigationBarLoading();
+          userInList = 1
+          break
+        }
+      }
+      if(userInList == 0){
+        that.getUserNameByID(id,userList)
+      }
+    }else{
+      let userList = []
+      that.getUserNameByID(id,userList)
+    }
+  },
+  getUserNameByID(id,userList){
+    let that = this;
+    wx.request({
+      url: 'https://www.nichuiniu.cn/wp-json/wp/v2/users/' + id,
+      header: {
+        'content-type': 'application/json',
+        'dataType': 'json'
+      },
+      success: function (res) {
+        var user = {id: id,name:res.data.name};
+        userList.push(user);
+        wx.setStorageSync('userList', userList);
+        that.setData({
+          author: res.data.name,
         });
         wx.hideLoading()
         wx.hideNavigationBarLoading();
@@ -129,7 +181,6 @@ Page({
   },
   onShareAppMessage: function (res) {
     return {
-      title: this.data.title,
       path: '/pages/index/index' + '?id=' + this.data.id,
     }
   },
